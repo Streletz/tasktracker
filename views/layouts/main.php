fb218d51d095a7fe5e5ffdabd4b5d6c3088b9bee
Streletz <streletzcoder@yandex.ru>
2018-09-03 02:20:19 +0300

    Версия 1.0 готова к выпуску

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -76,7 +76,8 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; Стрелец Coder, 2018</p>
+    	<p class="pull-left">Task tracker (версия <?= APP_VER ?>) </p>
+        <p class="pull-left">&copy; <a href="http://streletzcoder.ru/">Стрелец Coder</a>, 2018</p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
