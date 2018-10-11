51f206da9696be2bc825b576fff4c57f0b47538f
Streletz <streletzcoder@yandex.ru>
2018-10-11 18:13:16 +0300

    Удалён модуль CRM
    Функционал CRM удалён из основной ветки. Он сохранён в ветке 2.0.1 "до
    лучших времён".

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -43,8 +43,7 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             !Yii::$app->user->isGuest?['label' => 'Главная', 'url' => ['/site/index']]:'',
-            !Yii::$app->user->isGuest?['label' => 'Задачи', 'url' => ['/tasks/index']]:'',
-            ((!Yii::$app->user->isGuest) && ((Yii::$app->user->can('manager')) || (Yii::$app->user->can('admin'))))?['label' => 'Клиенты', 'url' => ['/crm/clients/index']]:'',            
+            !Yii::$app->user->isGuest?['label' => 'Задачи', 'url' => ['/tasks/index']]:'',                       
             ((!Yii::$app->user->isGuest) && (Yii::$app->user->can('admin')))?['label' => 'Пользователи', 'url' => ['admin/users/index']]:'',
             Yii::$app->user->isGuest ? (
                 ['label' => 'Войти', 'url' => ['/site/login']]
