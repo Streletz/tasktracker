ab42cf359508cf6f5e42ecf9b42b47146d623ea1
Streletz <streletzcoder@yandex.ru>
2018-11-28 15:01:06 +0300

    Перевод управления доступом на стандартный модуль RBAC (7й этап)
    Доработана инициализация ролей.

--- commands/RbacController.php
+++ commands/RbacController.php
@@ -4,25 +4,45 @@ namespace app\commands;
 
 use Yii;
 use yii\console\Controller;
+use app\modules\admin\models\Users;
 
 
 class RbacController extends Controller
 {
     public function actionInit()
     {
+        // Создание ролей.
+        // Роль администратора.
         $authManager = \Yii::$app->authManager;
         $role = Yii::$app->authManager->createRole('admin');
         $role->description = 'Администратор';
-        Yii::$app->authManager->add($role);
+        Yii::$app->authManager->add($role);        
         
-        $authManager = \Yii::$app->authManager;
+        // Роль менеджера.        
         $role = Yii::$app->authManager->createRole('manager');
         $role->description = 'Менеджер';
-        Yii::$app->authManager->add($role);
+        Yii::$app->authManager->add($role);        
         
+        // Роль пользователя.
         $role = Yii::$app->authManager->createRole('user');
         $role->description = 'Пользователь';
         Yii::$app->authManager->add($role);
+        
+        // Назначение ролей пользователям.
+        // Роль администратора.
+        $userRole = $userRole=Yii::$app->authManager->getRole('admin');
+        $user=Users::findOne(['username'=>'admin']);        
+        Yii::$app->authManager->assign($userRole, $user->id);
+        // Роль менеджера.
+        $userRole = $userRole=Yii::$app->authManager->getRole('manager');
+        $user=Users::findOne(['username'=>'manager']);
+        Yii::$app->authManager->assign($userRole, $user->id);
+        // Роль пользователя.
+        $userRole = $userRole=Yii::$app->authManager->getRole('user');
+        $user=Users::findOne(['username'=>'user']);
+        Yii::$app->authManager->assign($userRole, $user->id);
+        $user=Users::findOne(['username'=>'test']);
+        Yii::$app->authManager->assign($userRole, $user->id); 
     }
     
 }
