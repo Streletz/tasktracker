3e227975c76242578b8aa78ca612ca01c7203c5e
Streletz <streletzcoder@yandex.ru>
2019-11-23 09:43:18 +0300

    БД и почта
     - Шаблон для настроек почтовика для отправки писем;
     - Пользователям добавлен новый атрибут email.

---
+++ migrations/m191123_061607_email_for_users.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m191123_061607_email_for_users
+ */
+class m191123_061607_email_for_users extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m191123_061607_email_for_users cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m191123_061607_email_for_users cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
