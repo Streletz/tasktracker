5da0dfe9abc673e2ea8140318dfffaa872920a8f
Streletz <streletzcoder@yandex.ru>
2021-04-02 23:28:42 +0300

    4.0.0
     - Релиз 4.0.0.

--- web/index.php
+++ web/index.php
@@ -1,10 +1,10 @@
 <?php
 require_once "ReleaseType.php";
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-defined('RELEASE_TYPE') or define('RELEASE_TYPE', ReleaseType::STABLE);
+//defined('YII_DEBUG') or define('YII_DEBUG', true);
+//defined('YII_ENV') or define('YII_ENV', 'dev');
 
+defined('RELEASE_TYPE') or define('RELEASE_TYPE', ReleaseType::STABLE);
 defined('APP_VER') or define('APP_VER', '4.0.0');
 
 require __DIR__ . '/../vendor/autoload.php';
